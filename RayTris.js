0808449685259b15d286075b5f84f8099963032d
RTao <rtao@RTaos-MacBook-Air.local>
2014-08-21 12:23:28 -0400

    Added capability to highlight red

--- RayTris.js
+++ RayTris.js
@@ -1,9 +1,14 @@
 function addRow(tableID) {
     var tableRef = document.getElementById(tableID);
+    var hitRow = Math.floor(Math.random() * 10);
 
     for (var i = 0; i <10; i++) {
         var newRow = tableRef.insertRow();
         var newCell = newRow.insertCell();
+    
+        if (i === hitRow) {
+            newCell.style.background = "red";
+        }
     }
 }
 
